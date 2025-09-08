f3987bb14ace394a3e9e00d6eac61c03901987f0
Mauro Patrizio <maurodanielpatrizio@gmail.com>
2025-09-08 12:52:22 -0300

    first commit back

---
+++ Back_TravelBooking/src/main/java/com/travelbooking/Back_TravelBooking/BackTravelBookingApplication.java
@@ -0,0 +1,13 @@
+package com.travelbooking.Back_TravelBooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackTravelBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackTravelBookingApplication.class, args);
+	}
+
+}
---
+++ Back_TravelBooking/src/test/java/com/travelbooking/Back_TravelBooking/BackTravelBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelbooking.Back_TravelBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackTravelBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
